6bb9ebcdda62cbdc6383a3194d5116f6e19b7988
nkizito92 <nkizito57@gmail.com>
2020-08-14 08:24:35 -0500

    add scope for expensive_tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ActiveRecord::Base
     belongs_to :passenger, optional: true
     scope :cheapest, -> { order(price: :asc) }
     scope :expensive, -> { order(price: :desc) }
+    scope :expensive_tickets, -> { where('price > 1000') }
     validates :seat_num, :ticket_num, presence: true
     validates :price, numericality: { :greater_than_or_equal_to => 0 }
 end 
