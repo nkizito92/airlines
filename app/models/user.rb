8ac21f827eb46ecba4596dcb60dcf18a000cad09
nkizito92 <nkizito57@gmail.com>
2020-08-14 08:25:39 -0500

    validates password length has been added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
     has_many :passengers
     validates :username, :password, presence: true 
     validates :username, uniqueness: true
+    validates :password, {length: { in: 6..12 }}
     has_secure_password
 end
