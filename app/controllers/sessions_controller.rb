bc9636167641274ab1e57fa8906d0c5cded31892
nkizito92 <nkizito57@gmail.com>
2020-08-14 08:26:34 -0500

    password has a SecureRandom hex now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create 
         @user = User.find_or_create_by(uid: auth[:uid]) do |u|
             u.username = auth['info']['name']
-            u.password = SecureRandom.hex
+            u.password = SecureRandom.hex(6)
         end
           session[:user_id] = @user.id
           create_passenger_if_none_redirect
